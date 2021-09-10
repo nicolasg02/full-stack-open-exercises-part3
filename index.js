5d0ac9ca871124608e569c19dd46a65d9f317608
nicolasg02 <68668870+nicolasg02@users.noreply.github.com>
2021-09-10 05:11:12 -0300

    exercises

---
+++ index.js
@@ -0,0 +1,114 @@
+// Order:
+// 1. express server (app.use(express.json()))
+// 2. middleware (app.use(<middleware func>))
+// 3. routes
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`
+  );
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => {
+    return person.id === id;
+  });
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(400).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name is missing',
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing',
+    });
+  }
+
+  const personAlreadyExists = persons.some(person => person.name === body.name);
+
+  if (personAlreadyExists) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}..`);
+});
+
+// method:  GET
+// path:  /api/notes
+// body:  {}
+// ----
+// method:  POST
+// path:  /api/notes
+// body:  { content: 'VS Code REST client is pretty good', importance: false }
+// ----
