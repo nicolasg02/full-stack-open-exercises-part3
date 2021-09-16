67fbd8a5b760ecb3caec2820c82431a8fce59d9e
nicolasg02 <68668870+nicolasg02@users.noreply.github.com>
2021-09-16 09:37:25 -0300

    eslint config

--- index.js
+++ index.js
@@ -1,134 +1,136 @@
-const express = require('express'); // server
-const app = express();
-const cors = require('cors');
-app.use(cors());
-require('dotenv').config();
-const Person = require('./models/persons');
-const morgan = require('morgan'); // middleware
+const express = require('express') // server
+const app = express()
+const cors = require('cors')
+app.use(cors())
+require('dotenv').config()
+const Person = require('./models/persons')
+const morgan = require('morgan') // middleware
 
 morgan.token('body', (req, res) => {
-  return req.body;
-});
+  return req.body
+})
 
 const requestLogger = (tokens, req, res) => {
-  console.log('Method:', tokens.method(req, res));
-  console.log('Path:', tokens.url(req, res));
-  console.log('Status:', tokens.status(req, res));
-  console.log('Content Length:', tokens.res(req, res, 'content-length'));
-  console.log('Response Time:', tokens['response-time'](req, res), 'ms');
-};
+  console.log('Method:', tokens.method(req, res))
+  console.log('Path:', tokens.url(req, res))
+  console.log('Status:', tokens.status(req, res))
+  console.log('Content Length:', tokens.res(req, res, 'content-length'))
+  console.log('Response Time:', tokens['response-time'](req, res), 'ms')
+}
 
-app.use(express.static('build'));
-app.use(express.json());
-app.use(morgan(requestLogger));
+app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan(requestLogger))
 
 // get all contacts
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
+  const body = request.body
 
   if (body.name === undefined) {
-    return response.status(400).json({ error: 'name missing' });
+    return response.status(400).json({ error: 'name missing' })
   }
 
   if (body.name.length < 3) {
     return response
       .status(400)
-      .json({ error: 'Name must have at least 3 characters.' });
+      .json({ error: 'Name must have at least 3 characters.' })
   }
 
   if (body.number.length < 8) {
     return response
       .status(400)
-      .json({ error: 'Number must be at least 8 characters long.' });
+      .json({ error: 'Number must be at least 8 characters long.' })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then(savedPerson => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
     .catch(error => {
-      response.status(400).send({ error: 'Expected unique name.' });
-    });
-});
+      response.status(400).send({ error: 'Expected unique name.' })
+    })
+})
 
 // get contacts length
 app.get('/info', (request, response) => {
   Person.find({}, (err, results) => {
     response.send(
       `<p>Phonebook has info for ${results.length} people</p> ${new Date()}`
-    );
-  });
-});
+    )
+  })
+})
 
 // single contact url
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(400).end();
+        response.status(400).end()
       }
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // PUT
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // delete contact
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch(error => next(error));
-});
+    // eslint-disable-next-line no-undef
+    .catch(error => next(error))
+})
 
 const unknownEndPoint = (request, response) => {
-  response.status(400).send({ error: 'unknown endpoint' });
-};
+  response.status(400).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndPoint);
+app.use(unknownEndPoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/persons.js
+++ models/persons.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
+// eslint-disable-next-line no-undef
+const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 mongoose
   .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(result => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -28,16 +29,16 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     required: true,
   },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
